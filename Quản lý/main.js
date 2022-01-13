186ff70a1bd970a5881a752db0ef79b3e09866f2
TNT112000 <0972612200tuyen@gmail.com>
2022-01-13 13:05:50 +0700

    upload code

---
+++ Quản lý/main.js
@@ -0,0 +1,31 @@
+
+
+document.getElementById('tk').addEventListener("click", function () {
+    var clickTK1 = document.getElementById("click-tk1");
+    var clickTK2 = document.getElementById("click-tk2");
+    if (clickTK1.style.display === "none" && clickTK2.style.display === "none") {
+        clickTK1.style.display = "block";
+        clickTK2.style.display = "block";
+    }
+    else {
+        clickTK1.style.display = "none";
+        clickTK2.style.display = "none";
+    }
+})
+
+document.getElementById('btn1').addEventListener("click", function () {
+    var innerText = document.getElementsByClassName("sidebar-title");
+    for (var i = 0; i < innerText.length; i++) {
+        if (innerText[i].style.display === "none") {
+            innerText[i].style.display = "block";
+            document.getElementById('sidebar').style.width="240px";
+        }
+        else{
+            innerText[i].style.display = "none";
+            document.getElementById('sidebar').style.width="80px";
+        }
+    }
+
+  
+
+})
